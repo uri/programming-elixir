356f94d775b51639d65feea8e7a34572d11b98d3
Uri Gorelik <uri@uri-retro.local>
2016-01-11 22:24:01 -0500

    Finished chapter 6

--- chapter_06.exs
+++ chapter_06.exs
@@ -22,3 +22,29 @@ defmodule Chop do
     IO.puts "Is it #{guess}..."
   end
 end
+
+defmodule Times do
+  def double(n) do
+    n * 2
+  end
+
+  def triple(n) do
+    n * 3
+  end
+
+  def quadruple(n) do
+    double(n) * 2
+  end
+end
+
+defmodule UriMath do
+  def sum(0), do: 0
+  def sum(n) do
+    n + sum(n - 1)
+  end
+
+  def gcd(x,0), do: x
+  def gcd(x,y) do
+    gcd(y, rem(x,y))
+  end
+end
